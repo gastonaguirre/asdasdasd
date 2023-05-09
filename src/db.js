8ecab726903633f3c61fb43ce3e263f1b739c66a
gaston <ztonchyarg@hotmail.com>
2023-05-08 21:47:40 -0300

    Deploy

--- src/db.js
+++ src/db.js
@@ -27,7 +27,7 @@ let sequelize =
         },
         ssl: true
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
         logging: false,
         native: false
       });
